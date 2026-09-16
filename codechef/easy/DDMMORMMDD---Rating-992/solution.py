e1547223505f426cae5b5db6b617a75f97a2e159
Ashutosh Singh <ashutoshsingh4979@gmail.com>
2026-09-16 23:23:16 +0530

    ✅ DDMMORMMDD - Rating 992 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/easy/DDMMORMMDD---Rating-992/solution.py
@@ -0,0 +1,17 @@
+t = int(input())
+while t > 0:
+    s = input()
+    d1 = int(s[0:2])
+    d2 = int(s[3:5])
+    y = int(s[6:10])
+    leap = (y % 4 == 0 and (y % 100 != 0 or y % 400 == 0))
+    dim = [31, 29 if leap else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    ddmm = 1 <= d2 <= 12 and 1 <= d1 <= dim[d2 - 1]
+    mmdd = 1 <= d1 <= 12 and 1 <= d2 <= dim[d1 - 1]
+    if ddmm and mmdd:
+        print("BOTH")
+    elif ddmm:
+        print("DD/MM/YYYY")
+    else:
+        print("MM/DD/YYYY")
+    t -= 1
