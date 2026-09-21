1fd93dec37eddf07f48bbadb9b2882df5c001759
Ashutosh Singh <ashutoshsingh4979@gmail.com>
2026-09-21 21:12:05 +0530

    ✅ Find X Value of Array I | Medium | Java | Runtime: 13 ms (82.69%) | Memory: 92.1 MB | 2026-09-21

---
+++ leetcode/medium/3524-find-x-value-of-array-i/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] result = new long[k];
+        long[] dp = new long[k];
+        for (int num : nums) {
+            long[] next = new long[k];
+            int a = num % k;
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    next[(r * a) % k] += dp[r];
+                }
+            }
+            next[a]++;
+            for (int r = 0; r < k; r++) {
+                result[r] += next[r];
+            }
+            dp = next;
+        }
+        return result;
+    }
+}
