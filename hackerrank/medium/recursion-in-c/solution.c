ae1c5620a55a9055660240b5fc42c26a4b362f2c
Ashutosh Singh <ashutoshsingh4979@gmail.com>
2026-09-24 20:12:22 +0530

    ✅ Calculate the Nth term | Medium | C | Runtime: N/A | Memory: N/A | 2026-09-24

---
+++ hackerrank/medium/recursion-in-c/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int S(int n, int a, int b, int c) {
+    if (n == 1) return a;
+    if (n == 2) return b;
+    if (n == 3) return c;
+    return S(n-1, a, b, c) + S(n-2, a, b, c) + S(n-3, a, b, c);
+}
+
+int main() {
+    int n, a, b, c;
+    scanf("%d", &n);
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", S(n, a, b, c));
+    return 0;
+}
